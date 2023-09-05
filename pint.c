fef15d3cbb71c40ec6d0f3996b73a0db2962776a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-05 18:19:28 +0200

    edited pint to remove exess stack = null

--- pint.c
+++ pint.c
@@ -7,10 +7,11 @@
  */
 void f_pint(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || *stack == NULL)
+	if (*stack == NULL)
 	{
-		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		error = 1;
+		return;
 	}
 
 	printf("%d\n", (*stack)->n);
